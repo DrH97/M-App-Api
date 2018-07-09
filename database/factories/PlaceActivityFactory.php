9f34721ed56c26b12e0e8f9e3fabf51acef36ef0
DrH97 <jmnabangi@gmail.com>
2018-07-09 14:40:31 +0300

    Factory seeder update to accomodate clear db

--- database/factories/PlaceActivityFactory.php
+++ database/factories/PlaceActivityFactory.php
@@ -5,7 +5,7 @@ use Faker\Generator as Faker;
 $factory->define(App\PlaceActivity::class, function (Faker $faker) {
     return [
         //
-        'place_id' => $faker->numberBetween(1, 5),
+        'place_id' => $faker->randomElement($array = array ('2','12','22','32','42')),
         'price' => $faker->numberBetween(200, 6000)
     ];
 });
